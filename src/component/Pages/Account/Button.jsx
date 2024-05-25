3670640723fab6fed573213946cd9dcc963969ad
FORTRIX <mrrovergame@gmail.com>
2024-05-25 10:50:09 +0300

    Fix Button onClick action

--- src/component/Pages/Account/Button.jsx
+++ src/component/Pages/Account/Button.jsx
@@ -4,7 +4,13 @@ import styles from "./Button.module.scss";
 export default function Button({text, onClick}) {
   return (
     <div>
-      <button onClick={() => {onClick()}} href="" className={styles.user_button}>
+      <button 
+        onClick={() => {
+          if (onClick != undefined)
+            onClick()
+        }} 
+        className={styles.user_button}
+      >
         {text}
       </button>
     </div> 
